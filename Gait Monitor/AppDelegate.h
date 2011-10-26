5d7d2679887fb61ed26661a501c63aa280c03f56
Sebastian Weiß <coernel@Zeus.local>
2011-10-25 22:20:08 -0300

    Initial Commit

---
+++ Gait Monitor/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Gait Monitor
+//
+//  Created by Sebastian Weiß on 10/25/11.
+//  Copyright (c) 2011 Jade Hochschule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainViewController *mainViewController;
+
+@end
---
+++ Gait Monitor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gait Monitor
+//
+//  Created by Sebastian Weiß on 10/25/11.
+//  Copyright (c) 2011 Jade Hochschule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
